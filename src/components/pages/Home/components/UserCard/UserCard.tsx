7fce88a5808c070394dd756125c2c9938f5d69e5
Karel Klečka <kklecka@seznam.cz>
2023-11-24 17:02:08 +0100

    clean UserCard.tsx

--- src/components/pages/Home/components/UserCard/UserCard.tsx
+++ src/components/pages/Home/components/UserCard/UserCard.tsx
@@ -1,32 +1,25 @@
 import { ReactElement } from 'react';
-import { Button, Icon } from '@/components/shared';
+import { Icon } from '@/components/shared';
 import IUser from '@/model/IUser';
 import style from './UserCard.module.css';
 
-/*
-inteface IProps {
+interface IProps {
   user: IUser;
 }
 
-const UserCard = ({ user }: IProps):
-*/
-const UserCard = ({ user }: { user: IUser }): ReactElement => {
-  // const { name, email, phone, address: { geo: { lat, lng }} } = user;
-  const { lng, lat } = user.address.geo;
-  return <div className={[style.container].join(' ')}>
-    <h3>{user.name}</h3>
-    <div><Icon iconName="mail" className={style.inlineIcon} wrapperStyle="inline" />&nbsp;{user.email}</div>
-    <div><Icon iconName="call" className={style.inlineIcon} wrapperStyle="inline" />&nbsp;{user.phone}</div>
-    {/* Button v odkazu je pro přístupnost peklo. Tohle je špatně. */}
+const UserCard = ({ user }: IProps): ReactElement => {
+  const { name, email, phone, address: { geo: { lat, lng } } } = user;
+  return <div className={style.container}>
+    <h3>{name}</h3>
+    <div><Icon iconName="mail" className={style.inlineIcon} wrapperStyle="inline" />&nbsp;{email}</div>
+    <div><Icon iconName="call" className={style.inlineIcon} wrapperStyle="inline" />&nbsp;{phone}</div>
     <a
       href={`https://mapy.cz/turisticka?source=coor&id=${lng}%2C${lat}&x=${lng}&y=${lat}z=16`}
       target="_blank"
       rel="noreferrer"
       className={style.mapLink}
     >
-      <Button variant="secondary" className="!rounded-full">
-        <Icon iconName="search" className="h-5 inline" wrapperStyle="inline" /> Locate person
-      </Button>
+      <Icon iconName="search" className="h-5 inline" wrapperStyle="inline" /> Locate person
     </a>
   </div>;
 };
